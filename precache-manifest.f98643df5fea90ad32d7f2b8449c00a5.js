15b0670511b573f829938c730649954d268e9680
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-01-27 17:28:13 +0000

    Deploy Alarid/WeatherApp to github.com/Alarid/WeatherApp.git:gh-pages

---
+++ precache-manifest.f98643df5fea90ad32d7f2b8449c00a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a897e314e49f533c4300569dc9339f86",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "9e94a475b0286522d626",
+    "url": "/WeatherApp/static/css/main.b4d13d00.chunk.css"
+  },
+  {
+    "revision": "eee47da5c63d698377c0",
+    "url": "/WeatherApp/static/js/2.5af8e63d.chunk.js"
+  },
+  {
+    "revision": "43fafa47fcc31ad801a24b8595dc7aed",
+    "url": "/WeatherApp/static/js/2.5af8e63d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e94a475b0286522d626",
+    "url": "/WeatherApp/static/js/main.e4af4ab0.chunk.js"
+  },
+  {
+    "revision": "5fdeb778b69e8ff58bbb",
+    "url": "/WeatherApp/static/js/runtime-main.8c7ddbed.js"
+  }
+]);
